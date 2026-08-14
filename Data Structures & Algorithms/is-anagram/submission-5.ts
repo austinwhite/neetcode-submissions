20bca826ba2070626623e2151f2b7e351bc3e3c1
Austin White <austinlldb@gmail.com>
2026-08-14 18:07:12 -0500

    Add: is-anagram - submission-5

---
+++ Data Structures & Algorithms/is-anagram/submission-5.ts
@@ -0,0 +1,25 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {boolean}
+     */
+    isAnagram(s: string, t: string): boolean {
+        if (s.length !== t.length) { return false }
+
+        const array = new Array(26).fill(0);
+
+        for (const char of s) {
+            const key = char.charCodeAt(0) - 'a'.charCodeAt(0)
+            array[key] += 1
+        }
+
+        for (const char of t) {
+            const key = char.charCodeAt(0) - 'a'.charCodeAt(0)
+            array[key] -= 1
+            if (array[key] < 0) { return false }
+        }
+        
+        return true
+    }
+}
