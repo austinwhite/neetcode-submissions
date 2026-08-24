b616a17933700373cdb4d727a3f1ba63ad0f3375
Austin White <austinlldb@gmail.com>
2026-08-24 16:28:13 -0500

    Add: longest-common-prefix - submission-4

---
+++ Data Structures & Algorithms/longest-common-prefix/submission-4.ts
@@ -0,0 +1,17 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @return {string}
+     */
+    longestCommonPrefix(strs: string[]): string {
+        let prefix = strs[0]
+
+        for (const str of strs) {
+            while (!str.startsWith(prefix)) {
+                prefix = prefix.slice(0,-1)
+            }
+        }
+
+        return prefix
+    }
+}
